29f71c4232012376e2a133f3c1346515ccda755c
Ana Neto <a.neto@campus.fct.unl.pt>
2018-06-01 16:08:36 +0100

    Initial Commit

---
+++ DistractionJournal WatchKit Extension/InterfaceController.swift
@@ -0,0 +1,31 @@
+//
+//  InterfaceController.swift
+//  DistractionJournal WatchKit Extension
+//
+//  Created by Ana Neto on 01/06/2018.
+//  Copyright © 2018 Ana Neto. All rights reserved.
+//
+
+import WatchKit
+import Foundation
+
+
+class InterfaceController: WKInterfaceController {
+
+    override func awake(withContext context: Any?) {
+        super.awake(withContext: context)
+        
+        // Configure interface objects here.
+    }
+    
+    override func willActivate() {
+        // This method is called when watch view controller is about to be visible to user
+        super.willActivate()
+    }
+    
+    override func didDeactivate() {
+        // This method is called when watch view controller is no longer visible
+        super.didDeactivate()
+    }
+
+}
